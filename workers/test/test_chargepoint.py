5e05ed08a6dce94ead738972a9fe5b25c3656fc4
Duy Nguyen <nguyend@vmware.com>
2016-03-28 11:01:07 -0700

    Match VCR cassette by body

--- workers/test/test_chargepoint.py
+++ workers/test/test_chargepoint.py
@@ -7,7 +7,7 @@ import vcr
 
 charge_point = ChargePointConnection(CHARGEPOINT_USERNAME, CHARGEPOINT_PASSWORD)
 
-@vcr.use_cassette('fixtures/vcr_cassettes/test_charge_point_get_stations_info.yaml', filter_post_data_parameters=['user_name', 'user_password'])
+@vcr.use_cassette('fixtures/vcr_cassettes/test_charge_point_get_stations_info.yaml', filter_post_data_parameters=['user_name', 'user_password'], match_on=['body'])
 def test_get_stations_info():
    found_chargers = charge_point.get_vmware_stations("PG")
    len(found_chargers).should.equal(23)
--- workers/test/test_collector.py
+++ workers/test/test_collector.py
@@ -4,7 +4,7 @@ import pytest
 import sure
 import vcr
 
-@vcr.use_cassette('fixtures/vcr_cassettes/test_collector_get_current.yaml', filter_post_data_parameters=['user_name', 'user_password'])
+@vcr.use_cassette('fixtures/vcr_cassettes/test_collector_get_current.yaml', filter_post_data_parameters=['user_name', 'user_password'], match_on=['body'])
 def test_get_current():
    current_data = get_current()
    current_data['Central']['total_ports'].should.equal(13)
