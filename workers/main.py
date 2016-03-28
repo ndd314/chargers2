0db15aab547f7503cd108d87d36f0e389669d317
Duy Nguyen <ndd314@gmail.com>
2016-03-27 19:09:39 -0700

    Increase collector interval to five minutes

--- workers/main.py
+++ workers/main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
    p1 = Process(target=alerter.Alerter().main_loop, args=())
    p1.start()
 
-   p2 = Process(target=collector.collect_chargepoint_data_by_interval, args=(30,))
+   p2 = Process(target=collector.collect_chargepoint_data_by_interval, args=(300,))
    p2.start()
