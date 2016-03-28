0d118362e621eaa4e9311556efc68a0805b66cab
Duy Nguyen <nguyend@vmware.com>
2016-03-28 14:56:19 -0700

    Add more logging for easy sms

--- workers/easy_sms.py
+++ workers/easy_sms.py
@@ -1,15 +1,20 @@
-import json
-import urllib
+import logging
 import requests
 from config import *
 
 class EasySms:
    API_URL = EASYSMS_URL + "/messages"
 
+   logging.basicConfig()
+   logger = logging.getLogger()
+   logger.setLevel(logging.INFO)
+
    @classmethod
-   def send_sms(self, to=None, body=None):
+   def send_sms(cls, to=None, body=None):
       message = { 'to' : to, 'body' : body }
+      cls.logger.info("EasySms.send_sms(): texting {} with message".format(to, body))
       response = requests.session().post(url=EasySms.API_URL, data=message)
+
       # {
       #   "uid": "56f7d940d1471a0009000001",
       #   "to": "+14083935487",
@@ -19,4 +24,6 @@ class EasySms:
       #   "error_message": null,
       #   "c_at": "2016-03-27T12:59:44.424Z"
       # }
-      return response.json()['uid']
+
+      response_json = response.json()
+      cls.logger.info("EasySms.send_sms(): response = {}".format(response_json))
